5f74a6e217b3f1809847828dff0d16b110f11f3b
Azeez Adebayo <debaycisse@gmail.com>
2024-08-21 22:48:16 +0100

    apply a new logic to the handling of the lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,7 +10,7 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """Initializes an instance of this class"""
         super().__init__()
-        self.data_frequencies = {}
+        self.lru_queue = []
 
     def get_LRU(self):
         """
@@ -41,19 +41,18 @@ class LRUCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        if self.cache_data.get(key):
-            self.data_frequencies.pop(key)
-            self.cache_data.pop(key)
+
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            if key in self.cache_data:
+                if key in self.lru_queue:
+                    self.lru_queue.remove(key)
+            else:
+                lru_key = self.lru_queue[-1]
+                self.cache_data.pop(lru_key)
+                print('DISCARD: {0}'.format(lru_key))
+                self.lru_queue.remove(lru_key)
+        self.lru_queue.insert(0, key)
         self.cache_data.update({key: item})
-        self.data_frequencies.update({key: 1})
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            lru = self.get_LRU()
-            self.cache_data.pop(lru)
-            self.data_frequencies.pop(lru)
-            print('DISCARD: {0}'.format(lru))
-            for df in self.data_frequencies:
-                if df != key:
-                    self.data_frequencies[df] -= 1
 
     def get(self, key):
         """Retrieves data whose key is given from the cache
@@ -65,6 +64,8 @@ class LRUCache(BaseCaching):
             the item is returned if found, otherwise None is returned
         """
         data = self.cache_data.get(key)
-        if data is not None:
-            self.data_frequencies[key] += 1
+        if data:
+            if key in self.lru_queue:
+                self.lru_queue.remove(key)
+            self.lru_queue.insert(0, key)
         return data
