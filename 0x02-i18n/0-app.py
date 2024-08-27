a52d795a4cd71d74bc8ea076f57a3669ff063f80
Azeez Adebayo <debaycisse@gmail.com>
2024-08-27 16:24:17 +0000

    implement task 0
    0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module houses the definition of an instance of flask application"""
+from flask import (
+    Flask,
+    render_template
+)
+
+
+app: Flask = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Handles every request to the root's route"""
+    return render_template('0-index.html')
