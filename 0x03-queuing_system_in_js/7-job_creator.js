6b27a530e6f2e4f47c7aec2082976595bac10be8
Azeez Adebayo <debaycisse@gmail.com>
2024-09-24 15:48:37 +0100

    implement task 7
    8. Track progress and errors with Kue: Create the Job creator
    implement jobs creator that listens jobs' progress and error

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,74 @@
+const kue = require('kue');
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account',
+  },
+];
+
+const push_notification_code_2 = kue.createQueue();
+
+for (let i = 0; i < jobs.length; i += 1) {
+  const job = push_notification_code_2.create(
+    'push notification',
+    jobs[i]
+  )
+    .save((error) => {
+      if (!error) console.log(`Notification job created: ${job.id}`);
+    });
+};
+
+push_notification_code_2.on('job complete', (id, result) => {
+  console.log(`Notification job ${id} completed`);
+});
+
+push_notification_code_2.on('job failed', (id) => {
+  kue.Job.get(id, (error, job) => {
+    if (error) console.log(`Notification job JOB_ID failed: ${job._error}`);
+  });
+});
+
+push_notification_code_2.on('job progress', (id, progress) => {
+  console.log(`Notification job ${id} ${progress}% complete`);
+});
