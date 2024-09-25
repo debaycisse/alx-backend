5fc6298f3788c33ad10c36f3a3bcb0ab1c222d38
Azeez Adebayo <debaycisse@gmail.com>
2024-09-25 21:53:42 +0100

    implement task 13
    13. Can I have a seat?
    implement a seat reserve system that uses:
    	- redis to store seat reservation data,
    	- kue's queue to create tasks' or jobs' queue
    	- express' server api to expose the system's API for consumption

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,41 +1,87 @@
 import redis from 'redis';
 const kue = require('kue');
 const util = require('util');
+import { promisify } from 'util';
 const express = require('express');
 
 const client = redis.createClient();
 const queue = kue.createQueue();
 const app = express();
 const port = 1245;
-
 let numberOfSeatAvailable = 50;
-let reservationEnabled = true;  // it will be turn to false when no seat will be available
+let reservationEnabled = true;
 
-const reserveSeat = (number) => {
-  // Note that number must be a string
-  client.set('available_seats', number);
+const reserveSeat = async (number) => {
+  const asyncSet = promisify(client.set).bind(client);
+  const _value = await asyncSet('available_seat', String(number));
+  numberOfSeatAvailable -= 1;
+  return _value;
 };
 
 const getCurrentAvailableSeats = async () => {
   const getAsync = util.promisify(client.get).bind(client);
-  const result = await getAsync('available_seats');
-  return result;
+  const availableSeats = await getAsync('available_seats');
+
+  if(availableSeats !== null && availableSeats !== undefined) {
+    return availableSeats;
+  }
+  return numberOfSeatAvailable;
 };
 
 app.get('/available_seats', (req, res) => {
-  res.json({ "numberOfAvailableSeats": numberOfSeatAvailable });
+  res.json({ 'numberOfAvailableSeats': String(numberOfSeatAvailable) });
 });
 
 app.get('/reserve_seat', (req, res) => {
-  if(!reservationEnabled) {
-    return res.json({ "status": "Reservation are blocked" });
+  if(reservationEnabled === false) {
+    return res.json({ 'status': 'Reservation are blocked' });
+  } else {
+    try {
+      queue.on('job complete', (id, result) => {
+        console.log(`Seat reservation job ${id} completed`);
+      });
+
+      queue.on('job failed', (id, error) => {
+        console.log(`Seat reservation job ${id} failed: ${error}`);
+      });
+
+      const job = queue.create('reserve_seat')
+        .save((error) => {
+          if(error) {
+            console.error('error occured while saving job');
+            return;
+          } else {
+            return res.json({ 'status': 'Reservation in process' });
+          };
+        });
+    } catch(error) {
+      res.json({ 'status': 'Reservation failed' });
+    }
   }
-  queue.create('reserve_seat', );
+});
+
+app.get('/process', (req, res) => {
+  queue.process('reserve_seat', (job, done) => {
+    getCurrentAvailableSeats()
+      .then((result) => {
+        let availableSeatNum = Number.parseInt(result);
+        if(availableSeatNum <= 0) {
+          reservationEnabled = false;
+          const err = new Error('Not enough seats available');
+          done(err.message);
+        } else {
+          reserveSeat(availableSeatNum - 1)
+            .then(() => {
+              done();
+            });
+        };
+      });
+  });
+  res.json({ 'status': 'Queue processing' });
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-// When launching the application, set the number of available to 50
 module.exports = app;
